855431d9fe5e3be090fa96c00f42f8f87862f8f8
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-10-21 01:23:35 -0400

    added the post as well as the note ID to show the note

--- public/server.js
+++ public/server.js
@@ -9,7 +9,6 @@ var fs = require("fs");
 var app = express();
 var PORT = 3000;
 
-// const dataBase = require(path.join(__dirname, "db/db.json"));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +16,8 @@ app.use(express.json());
 app.use(express.static('assets'))
 
 // Middle stuff
+const dataBase = require(path.join("../db", "db.json"));
+let dataBaseObj = dataBase
 
 // html paths
 
@@ -36,16 +37,34 @@ app.get("/", function(req, res) {
 
   app.post("/api/notes", function(req, res) {
 
+
     let newNote = req.body;
+
+    id = dataBaseObj.length + 1;
+
+    console.log(id);
+
+    newNote.id = id;
 
     console.log(newNote);
 
-    // dataBase.push(newNote);
+    dataBase.push(newNote);
+
+    const dataBaseJSON = JSON.stringify(dataBase)
+
+    fs.writeFile("../db/db.json", dataBaseJSON, function (err) {
+      if (err) throw err;
+      console.log('Note Added!');
+    });
 
     // console.log(dataBase)
 
-    res.json(newNote);
+    res.json(dataBase);
 
+  })
+
+  app.delete("/api/notes/:id", function(req, res) {
+    console.log(req)
   })
   
 
@@ -53,6 +72,5 @@ app.get("/", function(req, res) {
 // =============================================================
 app.listen(PORT, function() {
     console.log("http://localhost:" + PORT);
-    console.log(__dirname);
   });
   
